6e23ad0f986122ddc51056050b71cefb88fc0a0c
Nikhil Arora <n.nikaro@gmail.com>
2018-01-13 13:13:50 +0530

    version 4

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-var todos =  ['item 1', 'item 2', 'item 3'];
-
-function displayTodos() {
-  console.log('My todos: ', todos);
-}
-
-function addTodo(todo) {
-  todos.push(todo);
-  displayTodos();
-}
-
-function changeTodo(position, newValue) {
-  todos[position] = newValue;
-  displayTodos();
-}
-
-function deleteTodo(position){
-  todos.splice(position, 1);
-  displayTodos();
-}
+var todoList = {
+  todos: [],
+  displayTodos: function() {
+    console.log('My todos: ', this.todos);
+  },
+  addTodo: function(todoText) {
+    this.todos.push({
+      todoText: todoText,
+      completed: false
+    });
+    this.displayTodos();
+  },
+  changeTodo: function(position, todoText) {
+    this.todos[position].todoText = todoText;
+    this.displayTodos();
+  },
+  deleteTodo: function(position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
+  },
+  toggleCompleted: function(position) {
+    var todo = this.todos[position];
+    todo.completed = !todo.completed;
+    this.displayTodos();
+  }
+};
