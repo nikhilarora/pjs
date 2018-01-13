b00ab3a145b8b087fc06710476d0b72dd901afa8
Nikhil Arora <n.nikaro@gmail.com>
2018-01-13 12:03:20 +0530

    version 2 complete

---
+++ app.js
@@ -0,0 +1,20 @@
+var todos =  ['item 1', 'item 2', 'item 3'];
+
+function displayTodos() {
+  console.log('My todos: ', todos);
+}
+
+function addTodo(todo) {
+  todos.push(todo);
+  displayTodos();
+}
+
+function changeTodo(position, newValue) {
+  todos[position] = newValue;
+  displayTodos();
+}
+
+function deleteTodo(position){
+  todos.splice(position, 1);
+  displayTodos();
+}
