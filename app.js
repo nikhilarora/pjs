fcf3a211f0015c6c762f0119df62a5a8dce0aaa1
Nikhil Arora <n.nikaro@gmail.com>
2018-01-14 11:52:25 +0530

    version 5

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var todoList = {
   todos: [],
   displayTodos: function() {
-    console.log('My todos: ', this.todos);
+    if (this.todos.length === 0){
+      console.log('Your todo list is empty!');
+    } else {
+      console.log('My todos: ');
+      for (var i = 0; i < this.todos.length; i++) {
+        if (this.todos[i].completed === true){
+          console.log('(x)', this.todos[i].todoText);
+        } else {
+          console.log('( )', this.todos[i].todoText);
+        }
+      }      
+    }
   },
   addTodo: function(todoText) {
     this.todos.push({
@@ -21,6 +32,32 @@ var todoList = {
   toggleCompleted: function(position) {
     var todo = this.todos[position];
     todo.completed = !todo.completed;
+    this.displayTodos();
+  },
+  toggleAll: function() {
+    var totalTodos = this.todos.length;
+    var completedTodos = 0;
+
+    // Get number of completed todos.
+    for (var i = 0; i < totalTodos; i++) {
+      if (this.todos[i].completed === true) {
+        completedTodos++;
+      }
+    }
+
+    // Case 1: If everything's true, make everything false.
+    if (completedTodos === totalTodos) {
+      for (var i = 0; i < totalTodos; i++) {
+        this.todos[i].completed = false;
+      }
+    // Case 2: Otherwise, make everything true.
+    } else {
+      for (var i = 0; i < totalTodos; i++){
+        this.todos[i].completed = true;
+      }
+    }
+    
+
     this.displayTodos();
   }
 };
