9829dc6030443f2e2c3abeacca5e868622431cc5
Nikhil Arora <n.nikaro@gmail.com>
2018-01-21 07:57:45 +0530

    version 7:

--- app.js
+++ app.js
@@ -56,8 +56,18 @@ var todoList = {
         this.todos[i].completed = true;
       }
     }
-    
+
 
     this.displayTodos();
   }
 };
+
+var displayTodosButton = document.getElementById('displayTodosButton');
+var toggleAllButton = document.getElementById('toggleAllButton');
+
+displayTodosButton.addEventListener('click', function() {
+  todoList.displayTodos();
+})
+toggleAllButton.addEventListener('click', function() {
+  todoList.toggleAll();
+})
