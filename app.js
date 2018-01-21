b5cb4cc94e0b67fd4e26e98eb19b26e03e76c371
Nikhil Arora <n.nikaro@gmail.com>
2018-01-21 10:49:55 +0530

    version 10

--- app.js
+++ app.js
@@ -89,8 +89,30 @@ var view = {
         todoTextWithCompletion = '( ) ' + todo.todoText;
       }
 
+      todoLi.id = i;
       todoLi.textContent = todoTextWithCompletion;
+      todoLi.appendChild(this.createDeleteButton());
       todosUl.appendChild(todoLi);
     }
+  },
+  createDeleteButton: function() {
+    var deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.className = 'deleteButton';
+    return deleteButton;
+  },
+  setupEventListners: function(){
+    var todosUl = document.querySelector('ul');
+    
+    todosUl.addEventListener('click', function(event) {
+      var elementClicked = event.target;
+    
+      if(elementClicked.className === 'deleteButton'){
+        handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      }
+    
+    });
   }
-}
+};
+
+view.setupEventListners();
